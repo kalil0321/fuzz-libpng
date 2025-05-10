2988e5ee66c3df3201d3a9455795863447107dcc
Hamza Remmal <hamza@remmal.net>
2025-05-10 13:09:24 +0200

    chore: more logs in the script

--- part1/run.w_corpus.sh
+++ part1/run.w_corpus.sh
@@ -1,21 +1,38 @@
+#!/bin/bash
+set -euo pipefail
+
+# CREATE A WORKING DIRECTORY
+export WORKDIR="$(mktemp -d)"
+echo "Using temp directory: $WORKDIR"
+cd "$WORKDIR"
+
 # configure the script
 export PROJECT=libpng
-export CORPUS=build/out/corpus
+export CORPUS="$WORKDIR/build/out/corpus"
 export HARNESS=libpng_read_fuzzer
-export REPOSITORY=https://github.com/hamzaremmal/fuzz-libpng.git
-export DURATION=4h
+export REPOSITORY=git@github.com:hamzaremmal/fuzz-libpng.git
+export DURATION=1m
+
+# Make sure the corpus directory exists
+echo "Making sure the corpus directory exists"
+mkdir -p "$CORPUS"
 
 # clone the oss-fuzz repository with corpus
-git clone $REPOSITORY -b oss-fuzz/with-corpus oss-fuzz
-# operate on the oss-fuzz repository
+echo "Cloning the oss-fuzz tree from $REPOSITORY"
+git clone "$REPOSITORY" -b oss-fuzz/with-corpus oss-fuzz
 cd oss-fuzz
 # build the image for libpng
-python3 infra/helper.py build_image --no-pull $PROJECT
+echo "Building images for project: $PROJECT"
+python3 infra/helper.py build_image --no-pull "$PROJECT"
 # build the fuzzers for libpng
-python3 infra/helper.py build_fuzzers --no-pull $PROJECT
+echo "Building fuzzers for project: $PROJECT"
+python3 infra/helper.py build_fuzzers --clean "$PROJECT"
 # run the fuzzer for 4 hours
-timeout --preserve-status $DURATION python3 infra/helper.py run_fuzzer $PROJECT $HARNESS --corpus-dir $CORPUS
+echo "Running fuzzer for project '$PROJECT' with harness '$HARNESS'"
+timeout --preserve-status "$DURATION" python3 infra/helper.py run_fuzzer "$PROJECT" "$HARNESS" --corpus-dir "$CORPUS"
 # build the fuzzer for coverage
-python3 infra/helper.py build_fuzzers --sanitizer coverage $PROJECT
+echo "Building the coverage fuzzer for project: $PROJECT"
+python3 infra/helper.py build_fuzzers --sanitizer coverage "$PROJECT"
 # build the coverage
-python3 infra/helper.py coverage $PROJECT --corpus-dir $CORPUS --fuzz-target $HARNESS
+echo "Building the coverage for project '$PROJECT' for harness '$HARNESS'"
+python3 infra/helper.py coverage "$PROJECT" --corpus-dir "$CORPUS" --fuzz-target "$HARNESS"
